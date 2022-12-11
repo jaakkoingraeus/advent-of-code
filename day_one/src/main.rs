ff1baf5795f725e5192a3d1e3e472c2c2026a343
Jaakko Ingraeus <jaakko@nacu.studio>
2022-12-12 00:23:38 +0200

    Complete 1.12.

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -6,40 +6,42 @@ fn main() {
     read_from_file_and_compare_calories(path);
 }
 
+fn return_elf(elf: i32, list_of_cals: &str) -> (i32, i64) {
+    let mut sum = 0;
+
+    let parsed_list = list_of_cals.split("\n");
+
+    for cal in parsed_list {
+        sum += cal.parse::<i64>().unwrap()
+    }
+
+    (elf, sum)
+}
+
 fn read_from_file_and_compare_calories(path: &str) {
     println!("Reading contents of: {:?}", path);
 
     let contents = fs::read_to_string(path).expect("Error reading the file");
 
     //Create an iterator through which we can loop through or collect() them to into a vector
-
     let iterator_of_elves = contents.split("\n\n");
 
     let mut max = 0;
     let mut max_index = 0;
 
-    let mut current_index = 0;
+    let mut number_of_times = 0;
+    let mut elves = iterator_of_elves.map(|x| { number_of_times += 1; return_elf(number_of_times, x) }).collect::<Vec<_>>();
 
-    for elf in iterator_of_elves {
-        //Loop through elf's calories and count them
+    //Sort elves based on number of calories
+    elves.sort_by(|a, b| b.1.cmp(&a.1));
 
-        let list_of_calories = elf.split("\n");
+    //Take top three
+    let top_three = elves.iter().take(3).collect::<Vec<_>>();
 
-        //Sum up calories
-        let mut sum_of_calories = 0;
-        for calories in list_of_calories {
-            sum_of_calories += calories.parse::<i64>().unwrap(); 
-        }
+    println!("Top three elves: {:?}", top_three.iter().map(|x| x.0).collect::<Vec<_>>());
 
-        //Compare to the current max
-        if sum_of_calories > max {
-            max = sum_of_calories;
-            max_index = current_index;
-        } 
+    let total_of_top = top_three.iter().fold(0, |acc, x| acc + x.1);
 
-        current_index += 1;
-    }
-
-    println!("Elf number {:?} has the most calories. Number of calories is: {:?}", max_index, max)
+    println!("Their total calories: {:?}", total_of_top)
 
 }
